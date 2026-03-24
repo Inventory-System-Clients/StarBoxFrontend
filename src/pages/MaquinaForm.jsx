b8c5e797e083f5ec6caa0a9157673e474b6158fb
Tuco2006 <mateussmcruz@icloud.com>
2026-03-24 16:19:49 -0300

    adicionando mensagem correta movimentação

--- src/pages/MaquinaForm.jsx
+++ src/pages/MaquinaForm.jsx
@@ -427,7 +427,7 @@ export function MaquinaForm() {
 
                 <div>
                   <label className="block text-sm font-semibold text-gray-700 mb-2">
-                    Valor da Ficha (R$)
+                    Valor da Jogada (R$)
                   </label>
                   <input
                     type="number"
